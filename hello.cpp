8a73fb82cc4b18e3cdc388372d5156250a13ab42
Karim Naqvi <naqvik@uregina.ca>
2022-09-19 15:57:29 -0600

    Print some sines

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, Earth!\n";
+    double x = 0.0;
+    for (int i=1; i<10; x+=.001, ++i) {
+        std::cout << x << " ,"<< sin(x) << "\n";
+    }
 }
